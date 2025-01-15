e58bd4c12ec41c5a497c0d6380b3b3d87fae62b6
tn-py <tirso.an08@gmail.com>
2025-01-14 23:18:14 -0500

    update

--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -21,7 +21,7 @@ const About = () => {
 
                 <div className="mx-auto flex w-[100%] flex-col lg:max-w-[1200px] lg:flex-row lg:gap-20">
                     <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-medium leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[20px] md:leading-relaxed lg:mb-16 lg:max-w-[90%] lg:text-[24px]">
-                        <AnimatedBody text="I'm an aspiring Full-Stack Web Developer based in Mexico City. I have extensive experience in Shopify theme development, specializing in creating seamless digital solutions that enhance user experience and operational efficiency. I'm proficient in building and optimizing Shopify themes, pages, scripts, and automations. Utilizing tools like Shopify Flow and Matrixify, I perform mass uploads and updates to streamline processes and reduce operational costs." />
+                        <AnimatedBody text="I'm an aspiring Full-Stack Web Developer based in Miami, FL. I have extensive experience in Shopify theme development, specializing in creating seamless digital solutions that enhance user experience and operational efficiency. I'm proficient in building and optimizing Shopify themes, pages, scripts, and automations. Utilizing tools like Shopify Flow and Matrixify, I perform mass uploads and updates to streamline processes and reduce operational costs." />
 
                         <AnimatedBody
                             delay={0.1}
