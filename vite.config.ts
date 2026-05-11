b5b8c6d6c37c7bf0d3361295f8fb636cf597d2e9
Tirso Navarro <tirso.an08@gmail.com>
2026-05-11 12:38:13 -0400

    feat: update projects section

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 export default defineConfig({
+  server: {
+    allowedHosts: [".tunnelmole.net"],
+  },
   plugins: [
     mdx({
       remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
